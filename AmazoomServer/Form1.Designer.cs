3962fb670674b65bb8aaf698aadc20e015ee1f7d
Gurmehak Pannu <gm.pannu41@gmail.com>
2022-01-03 19:59:47 -0800

    First Commit

---
+++ AmazoomServer/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace AmazoomServer
+{
+    partial class FormServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.buttonPort = new System.Windows.Forms.Button();
+            this.labelWarehouse = new System.Windows.Forms.Label();
+            this.labelClient = new System.Windows.Forms.Label();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBoxWarehouse = new System.Windows.Forms.TextBox();
+            this.textBoxClient = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(58, 36);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(135, 20);
+            this.textBoxPort.TabIndex = 0;
+            this.textBoxPort.Text = "8911";
+            // 
+            // labelPort
+            // 
+            this.labelPort.Location = new System.Drawing.Point(12, 37);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(40, 20);
+            this.labelPort.TabIndex = 1;
+            this.labelPort.Text = "Port";
+            this.labelPort.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonPort
+            // 
+            this.buttonPort.Location = new System.Drawing.Point(199, 10);
+            this.buttonPort.Name = "buttonPort";
+            this.buttonPort.Size = new System.Drawing.Size(75, 47);
+            this.buttonPort.TabIndex = 2;
+            this.buttonPort.Text = "Start";
+            this.buttonPort.UseVisualStyleBackColor = true;
+            this.buttonPort.Click += new System.EventHandler(this.buttonPort_Click);
+            // 
+            // labelWarehouse
+            // 
+            this.labelWarehouse.Location = new System.Drawing.Point(12, 86);
+            this.labelWarehouse.Name = "labelWarehouse";
+            this.labelWarehouse.Size = new System.Drawing.Size(100, 20);
+            this.labelWarehouse.TabIndex = 4;
+            this.labelWarehouse.Text = "Warehouse";
+            this.labelWarehouse.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // labelClient
+            // 
+            this.labelClient.Location = new System.Drawing.Point(146, 86);
+            this.labelClient.Name = "labelClient";
+            this.labelClient.Size = new System.Drawing.Size(100, 20);
+            this.labelClient.TabIndex = 8;
+            this.labelClient.Text = "Client";
+            this.labelClient.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // labelIP
+            // 
+            this.labelIP.Location = new System.Drawing.Point(12, 11);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(40, 20);
+            this.labelIP.TabIndex = 11;
+            this.labelIP.Text = "IP";
+            this.labelIP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(58, 12);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(135, 20);
+            this.textBoxIP.TabIndex = 12;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // textBoxWarehouse
+            // 
+            this.textBoxWarehouse.Location = new System.Drawing.Point(12, 109);
+            this.textBoxWarehouse.Multiline = true;
+            this.textBoxWarehouse.Name = "textBoxWarehouse";
+            this.textBoxWarehouse.ReadOnly = true;
+            this.textBoxWarehouse.Size = new System.Drawing.Size(125, 124);
+            this.textBoxWarehouse.TabIndex = 13;
+            // 
+            // textBoxClient
+            // 
+            this.textBoxClient.Location = new System.Drawing.Point(149, 109);
+            this.textBoxClient.Multiline = true;
+            this.textBoxClient.Name = "textBoxClient";
+            this.textBoxClient.ReadOnly = true;
+            this.textBoxClient.Size = new System.Drawing.Size(125, 124);
+            this.textBoxClient.TabIndex = 14;
+            // 
+            // FormServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 245);
+            this.Controls.Add(this.textBoxClient);
+            this.Controls.Add(this.textBoxWarehouse);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.labelIP);
+            this.Controls.Add(this.labelClient);
+            this.Controls.Add(this.labelWarehouse);
+            this.Controls.Add(this.buttonPort);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.textBoxPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormServer";
+            this.Text = "Amazoom Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Button buttonPort;
+        private System.Windows.Forms.Label labelWarehouse;
+        private System.Windows.Forms.Label labelClient;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox textBoxWarehouse;
+        private System.Windows.Forms.TextBox textBoxClient;
+    }
+}
+
---
+++ AmazoomServer/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleTCP;
+
+namespace AmazoomServer
+{
+    public partial class FormServer : Form
+    {
+        public class ProductData
+        {
+            public string Product
+            { get; }
+
+            public int Quantity
+            { get; }
+
+            public ProductData(string product, int quantity)
+            {
+                this.Product = product;
+                this.Quantity = quantity;
+            }
+        }
+
+        private SimpleTcpServer server;
+        private readonly List<int> clientIds;
+        private readonly Dictionary<int, List<ProductData>> warehouseInventory;
+
+        public FormServer()
+        {
+            InitializeComponent();
+
+            this.clientIds = new List<int>();
+            this.warehouseInventory = new Dictionary<int, List<ProductData>>();
+        }
+
+        private void buttonPort_Click(object sender, EventArgs e)
+        {
+            IPAddress address;
+            try
+            {
+                address = IPAddress.Parse(textBoxIP.Text);
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Error: Incorrect IP Format");
+                return;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error: Incorrect IP Format");
+                return;
+            }
+
+            int port;
+            if (string.IsNullOrEmpty(textBoxPort.Text))
+            {
+                MessageBox.Show("Please fill in Port Number");
+                return;
+            }
+            try
+            {
+                port = Convert.ToInt32(textBoxPort.Text);
+                if (port == 0)
+                {
+                    MessageBox.Show("Error: Invalid Port");
+                    return;
+                }
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Error: Incorrect Port Format");
+                return;
+            }
+
+            try
+            {
+                this.server = new SimpleTcpServer().Start(address, port);
+                this.server.Delimiter = 0x13;  // enter
+                this.server.StringEncoder = Encoding.UTF8;
+                this.server.DelimiterDataReceived += ServerDataReceived;
+                buttonPort.Enabled = false;
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Error: Cannot connect to network.");
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            textBoxWarehouse.Clear();
+            foreach (var warehouseId in this.warehouseInventory.Keys)
+            {
+                textBoxWarehouse.Text += warehouseId.ToString() + "\r\n";
+            }
+
+            textBoxClient.Clear();
+            foreach (var clientId in this.clientIds)
+            {
+                textBoxClient.Text += clientId.ToString() + "\r\n";
+            }
+        }
+
+        private void broadcast(string command, string sender, string receiver,
+            string payload)
+        {
+            if (string.IsNullOrEmpty(sender))
+            {
+                sender = "-";
+            }
+            if (string.IsNullOrEmpty(receiver))
+            {
+                receiver = "-";
+            }
+            if (string.IsNullOrEmpty(payload))
+            {
+                payload = "-";
+            }
+            this.server.BroadcastLine(command + "/"
+                + sender + "/"
+                + receiver + "/"
+                + payload);
+        }
+
+        private void sendInventory()
+        {
+            string payload = "";
+            // Assume inventory of each warehouse is unique
+            foreach (var inventory in this.warehouseInventory)
+            {
+                foreach (var productData in inventory.Value)
+                {
+                    payload += productData.Product
+                        + "*"
+                        + productData.Quantity.ToString()
+                        + ",";
+                }
+            }
+            broadcast("InventoryToClient", null, null, payload);
+            return;
+        }
+
+        private void sendDeletedProduct(string name, int quantity)
+        {
+            string payload = name + "*" + quantity.ToString();
+            broadcast("ClientDeleteProduct", null, null, payload);
+            return;
+        }
+
+        private void ServerDataReceived(object sender, SimpleTCP.Message e)
+        {
+            string[] message = e.MessageString.Split('/');
+            if (message.Length != 4)
+            {
+                return;
+            }
+            string messageCommand = message[0];
+            string messageSender = message[1];
+            string messageReceiver = message[2];
+            string messagePayload = message[3];
+
+            int clientId;
+            int warehouseId;
+            switch (messageCommand)
+            {
+                case "ClientStart":
+                    clientId = Convert.ToInt32(messageSender);
+                    if (clientId != 0 &&
+                        !this.clientIds.Contains(clientId))
+                    {
+                        this.clientIds.Add(clientId);
+                    }
+                    sendInventory();
+                    break;
+                case "WarehouseStart":
+                    warehouseId = Convert.ToInt32(messageSender);
+                    if (warehouseId != 0 &&
+                        !this.warehouseInventory.Keys.Contains(warehouseId))
+                    {
+                        string[] productQuantities = messagePayload.Split(',');
+                        string[] productQuantity;
+                        List<ProductData> inv = new List<ProductData>();
+                        for (int i = 0; i < productQuantities.Length; i++)
+                        {
+                            productQuantity = productQuantities[i].Split('*');
+                            if (productQuantity.Length == 2)
+                            {
+                                string product = productQuantity[0];
+                                string quantity = productQuantity[1];
+                                inv.Add(new ProductData(
+                                    product, Convert.ToInt32(quantity)));
+                            }
+                        }
+                        this.warehouseInventory.Add(
+                            Convert.ToInt32(messageSender), inv);
+                        // Update client regarding the inventory
+                        sendInventory();
+                    }
+                    break;
+                case "WarehouseNewProduct":
+                    warehouseId = Convert.ToInt32(messageSender);
+                    if (this.warehouseInventory.Keys.Contains(warehouseId))
+                    {
+                        List<ProductData> inv = 
+                            this.warehouseInventory[warehouseId];
+                        string[] productQuantity = messagePayload.Split('*');
+                        if (productQuantity.Length == 2)
+                        {
+                            string product = productQuantity[0];
+                            string quantity = productQuantity[1];
+                            inv.Add(new ProductData(
+                                product, Convert.ToInt32(quantity)));
+                        }
+
+                        this.warehouseInventory[warehouseId] = inv;
+                        sendInventory();
+                    }
+                    break;
+                case "WarehouseDeleteProduct":
+                    warehouseId = Convert.ToInt32(messageSender);
+                    if (this.warehouseInventory.Keys.Contains(warehouseId))
+                    {
+                        List<ProductData> inv =
+                            this.warehouseInventory[warehouseId];
+
+                        string[] productQuantity = messagePayload.Split('*');
+                        string product = productQuantity[0];
+                        string quantity = productQuantity[1];
+
+                        int j = 0;
+                        foreach (ProductData productData in inv)
+                        {
+                            if (productData.Product == product)
+                            {
+                                quantity = productData.Quantity.ToString();
+
+                                //inv.Remove(new ProductData(
+                                //    product, Convert.ToInt32(quantity)));
+                                inv.RemoveAt(j);
+
+                                this.warehouseInventory[warehouseId] = inv;
+                                sendDeletedProduct(
+                                    product, Convert.ToInt32(quantity));
+                                break;
+                            }
+                            j++;
+                        }
+                    }
+                    break;
+                case "OrderFromClient":
+                    // No available warehouse
+                    if (this.warehouseInventory.Count == 0)
+                    {
+                        broadcast("OrderToClient", null, messageSender,
+                            "Denied");
+                        break;
+                    }
+
+                    // Assume each order contains one product only
+                    string[] productDataOrder = messagePayload.Split('*');
+                    if (productDataOrder.Length < 2)
+                    {
+                        broadcast("OrderToClient", null, messageSender,
+                            "Denied");
+                        break;
+                    }
+
+                    // Check which warehouse to send order
+                    bool orderSent = false;
+                    foreach (var inventory in this.warehouseInventory)
+                    {
+                        foreach (var productData in inventory.Value)
+                        {
+                            if (productData.Product == productDataOrder[0])
+                            {
+                                broadcast("OrderToWarehouse", messageSender,
+                                    inventory.Key.ToString(), messagePayload);
+                                orderSent = true;
+                                break;
+                            }
+                        }
+                        if (orderSent)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Order cannot be fulfilled
+                    if (!orderSent)
+                    {
+                        broadcast("OrderToClient", null, messageSender,
+                            "Denied");
+                    }
+                    break;
+                case "OrderFromWarehouse":
+                    broadcast("OrderToClient", null,
+                        messageReceiver, messagePayload);
+                    break;
+                case "ClientStop":
+                    clientId = Convert.ToInt32(messageSender);
+                    this.clientIds.Remove(clientId);
+                    break;
+                case "WarehouseStop":
+                    warehouseId = Convert.ToInt32(messageSender);
+                    this.warehouseInventory.Remove(warehouseId);
+                    sendInventory();
+                    break;
+            }
+        }
+    }
+}
